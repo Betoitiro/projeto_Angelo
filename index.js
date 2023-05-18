9de0eed9f90c8022395f9bb73428c44dc42c4b1a
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-05-17 23:59:41 -0300

    basico de html, css, e aplicação js

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
+
 let usuarios = []
 
 function cadastrar(){
     let nome = document.getElementById("nome").value;
     let Senha = document.getElementById("password").value;
 
-    let usuario = (nome, Senha);
+    let usuario = {
+        nome: nome,
+        Senha: Senha,
+        carrinho : [],
+        valorCompra: 0,
+        pedido: " "
+    };
     usuarios.push(usuario);
 
-    document.getElementById("nome-usuario").textContent = nome
+    document.getElementById("nome-usuario").textContent = nome;
 }
+function adicionarCarrinho(item){
+    let nomeuser = document.getElementById("nome-usuario").textContent;
+    let user = usuarios.find(u => u.nome === nomeuser);
+
+    if(user){
+        usuario.carrinho.push(item);
+        usuario.valorCompra += item.preco;
+
+        exibirCarrinho(usuario.carrinho);
+        exibirValorCompra(usuario.valorCompra);
+    }
+}
+
+function fazerPedido(){
+    let nomeuser = document.getElementById("nome-usuario").textContent;
+    let user = usuarios.find(u => u.nome === nomeuser);
+
+    if(usuario){
+        usuario.carrinho=[]
+        usuario.valorCompra = 0;
+        usuario.pedido = " ";
+
+        exibirCarrinho(usuario.carrinho);
+        exibirValorCompra(usuario.valorCompra);
+        exibirPedido(usuario.pedido);
+    }
+}
+
+function exibirCarrinho(carrinho) {
+    let carrinhoElement = document.getElementById("carrinho");
+    carrinhoElement.innerHTML = "";
+  
+    carrinho.forEach(item => {
+      let itemElement = document.createElement("li");
+      itemElement.textContent = item.nome;
+      carrinhoElement.appendChild(itemElement);
+    });
+  }
+
+  function exibirValorCompra(valor){
+
+  }
+
+  function exibirPedido(pedido){
+    
+  }
